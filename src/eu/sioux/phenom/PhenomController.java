3bdb85d28665a0d39be251ab8b3c9d2b1320d6b1
Rico Huijbers <rix0rrr@gmail.com>
2011-10-14 23:50:51 +0200

    First commit from Boy/Nicky's project

---
+++ src/eu/sioux/phenom/PhenomController.java
@@ -0,0 +1,118 @@
+package eu.sioux.phenom;
+
+import java.util.Vector;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+/*
+ * This class handles the logic when the user presses the
+ * power button
+ */
+public class PhenomController {
+
+	String TAG = PhenomController.class.getSimpleName();
+	
+	/*
+	 * these static ints (constants) are being used by the message handler
+	 */
+	final static int GET_OPERATIONAL_MODE = 0;
+	public final static int GET_OPERATIONAL_MODE_RESULT = 1;
+
+	/*
+	 * This variable will be used to store the reference to the handler of
+	 * the parent. This way we can asynchronously report a status update  
+	 */
+	Handler mFeedbackHandler = null;
+	
+	public PhenomController(Handler handler) {
+		mFeedbackHandler = handler;
+	}
+
+	void getOperationalMode() {
+
+		/*
+		 * 
+		 */
+		new asyncPowerHandling().execute(GET_OPERATIONAL_MODE);
+	}
+
+	private static final String WebServiceSoapAction = "";
+	private static final String WebServiceMethodName = "GetInstrumentMode"; //GetOperationalMode
+	private static final String WebServiceNameSpace  = "http://tempuri.org/om.xsd";
+	private static final String WebServiceUrl        = "http://192.168.5.133:8888/";			
+	
+	private void GetInstrumentMode()
+	{	
+        SoapObject request = new SoapObject(WebServiceNameSpace, WebServiceMethodName);        
+        //request.addProperty("PropertyName", "PropertyValue");
+        SoapSerializationEnvelope soapEnvelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);		
+        soapEnvelope.setOutputSoapObject(request);
+        
+        HttpTransportSE hts = new HttpTransportSE(WebServiceUrl);
+        try {
+			hts.call(WebServiceSoapAction, soapEnvelope);
+			
+			Vector soapResult     = (Vector)soapEnvelope.getResponse();
+			String instrumentMode = ((SoapPrimitive)soapResult.get(0)).toString();
+
+			Message msg = new Message();
+			msg.obj = instrumentMode;
+			msg.what = GET_OPERATIONAL_MODE_RESULT;
+			mFeedbackHandler.sendMessage(msg);
+			
+		} catch (Exception e) {
+			Log.e("ERROR", e.getMessage());				
+		}	        
+	}
+	
+	/*
+	 * This calls is used to do an asynchronous task
+	 */
+	private class asyncPowerHandling extends AsyncTask<Integer, Void, Void> {
+
+		/*
+		 * This function is called before doInBackground().
+		 * 
+		 * it runs in the UI thread!
+		 */
+		@Override
+		protected void onPreExecute() {
+
+		}
+
+		/*
+		 * this function does the actual work that has to be done
+		 * asynchronously.
+		 * 
+		 * changes to the UI are not allowed here, as it does NOT run in the UI
+		 * thread!
+		 */
+		@Override
+		protected Void doInBackground(Integer... params) {
+			if (params[0] == GET_OPERATIONAL_MODE) {
+				GetInstrumentMode();
+			}
+			return null;
+		}
+		
+		/*
+		 * This function is called after doInBackground(). The argument is the
+		 * return value of doInBackground().
+		 * 
+		 * it runs in the UI thread!
+		 */
+		@Override
+		protected void onPostExecute(Void result) {
+		}
+
+	}
+}
---
+++ src/eu/sioux/phenom/PhenomRemoteControlActivity.java
@@ -0,0 +1,108 @@
+package eu.sioux.phenom;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PhenomRemoteControlActivity extends Activity {
+	/** Called when the activity is first created. */
+
+	PhenomController phenomController = null;
+	private Button mGetOperationalModeBtn = null;
+	private TextView mOperationalModeTextView = null;
+
+	final static String TAG = PhenomController.class.getSimpleName();
+	/*
+	 * Called when the activity is created
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		
+		/*
+		 * here you set the content view. This is actually the user interface.
+		 * 
+		 * "R.layout.main" refers to "res/layout/main.xml" Android creates an
+		 * R.java file in the "gen"-directory that creates these ID's. This is
+		 * done for every resource in your project when compiling.
+		 */
+		setContentView(R.layout.main);
+
+		/*
+		 * the PhenomController class has been created to communicate with the Phenom
+		 */
+		phenomController = new PhenomController(mHandler);
+
+		/*
+		 * as we have set the layout above via setContentView, the items in that
+		 * layout can be called by findViewById(). Let's get the button that gets the
+		 * operational mode of the system
+		 */
+		mGetOperationalModeBtn = (Button) findViewById(R.id.getOperationalModeButton);
+		
+		/*
+		 * here is the textview where we show the current operational mode in
+		 */
+		mOperationalModeTextView = (TextView) findViewById(R.id.getOperationalModeTextView);
+
+		/*
+		 * now when the button gets pressed, we want the PhenomController to get the
+		 * current operational state. Here we create an onClickListener and connect it
+		 * to the button
+		 */
+		mGetOperationalModeBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				/*
+				 * this function is non-blocking. We will be receiving the status via the
+				 * handler passed to the PhenomController constructor
+				 */
+				phenomController.getOperationalMode();
+			}
+		});
+		
+	}
+	/*
+	 * this is the message handler that we passed on to the PhenomController object.
+	 * It will send messages when it has new data. It doesn't matter from which thread
+	 * the message is send, all messages will always be handled here in the same thread.
+	 * 
+	 * This is imported when changing UI. You are only allowed to change UI stuff in the
+	 * UI thread. So handling UI changes in this handler is safe as it is always being done
+	 * in the UI thread.
+	 */
+	Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			/*
+			 * messages send via the Handler methods, like sendMessage() or sendEmptyMessage, 
+			 * will end up in this handler. The message ID can be derived from msg.what. So
+			 * here we the handle different messages.
+			 * 
+			 * Only one at the moment...
+			 */
+			if (msg.what == PhenomController.GET_OPERATIONAL_MODE_RESULT) {
+				/*
+				 * Hey! We received operational mode info, show it in the textview!
+				 */
+				String result = (String) msg.obj;
+				mOperationalModeTextView.setText(result);				
+			} else {
+				/*
+				 * check the logcat for this message in case of problems to see if you message
+				 * is being handled.
+				 */
+				Log.e(TAG, "Unhandled message from PhenomController: " + Integer.toString(msg.what));
+			}
+		}
+	};
+}
+
